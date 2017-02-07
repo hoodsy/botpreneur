b268daa443e58d8cde36c68c7d0f86996ae42564
Logan Bernard <bernard.logan4@gmail.com>
2017-02-06 21:32:17 -0500

    trying styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import picture from './assets/images/profile_pic.jpg';
+import logo from './assets/logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <div className="picture">
-          <img className="profile-picture" src={picture} alt="Logan Bernard, Botpreneur" />
-        </div>
-        <h1></h1>
+        <section className="bio-container">
+          <div>
+            <img className="profile-picture" src={picture} alt="Logan Bernard, Botpreneur" />
+          </div>
+          <div className="column">
+            <h1 className="name">Logan Bernard</h1>
+            <img className="logo" src={logo} alt="Botpreneur" />
+            <p></p>
+          </div>
+        </section>
       </div>
     );
   }
